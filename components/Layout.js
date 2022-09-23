5eb5c8e919a578e863ca47afcbd6520b55f094c0
nivi1998 <niveditasinghrajput801@gmail.com>
2022-09-23 19:51:24 +0530

    install next auth package for login and conect to database and install react-toastify, in layout we show user name if it is loged in, add session provider in app.js file to provide session of all pages, in login page we send ajax request to sign in api to authenticate the user, create nextauth.js to provide credential authentication using mongo db database, create error function to get error message

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { Children, useEffect } from 'react';
 import { useContext, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Store } from '../utils/Store';
 
 function Layout({ title, children }) {
+  const { status, data: session } = useSession();
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -18,7 +22,7 @@ function Layout({ title, children }) {
         <meta name="description" content="E-commerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <ToastContainer position="bottom-center" limit={1} />
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
@@ -36,9 +40,16 @@ function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
